6b1035a2ca0bbf4b63d223a82548d353b6fdf72a
Karlduino <karlduino@gmail.com>
2014-08-31 19:35:53 -0500

    Add code for buttons and LEDs; working on microphone

--- Skeet.ino
+++ Skeet.ino
@@ -9,40 +9,142 @@
  *      - laser module mount   sparkfun COM-08674
  *      - electret microphone   sparkfun BOB-09964
  *      - Arduino (Yun)
+ *      - two LEDs with 470 Ohm resistors
  *
  */
 
 #include <Servo.h>
+#include <math.h>
 
-Servo myservo;
-const int servopin=10;
-const double maxpos = 2;
-const int n_steps = 100;
-const double sec_per_sweep = 1.5;
-const int delay_between = 2000;
+Servo leftservos[2];
 
-double servopos = -maxpos;
+const int SERVO_PINS[2] = {9, 10}; /* side/side = 9, up/down=10 */
+const int LED_PINS[2] = {6, 5};    /* left=6, right=5 */
+const int BUTTON_PIN = 8;              /* button to switch left/right */
+const int LASER_PIN = 7;
+const int MIC_PIN = A2;
+
+const int ANGLES[2] = {135, 105}; /* up/down angles for high/left, low/right */
+const int CENTER[2] = {90, 135};
+const double MAXPOS[2] = {-3.0, 3.0}; /* max position to left and right */
+
+const int DELAY_BEFORE = 1000; /* delay between "pull" and fly */
+const int TIME_TO_FLY  = 1500; /* time to fly */
+const int DELAY_AFTER  = 1000; /* delay after fly */
+const int N_STEPS = 100;       /* no. steps to fly */
+
+/* button and skeet state */
+int button_press = HIGH; /* HIGH = not pressed, because of pull-up resistor */
+int prev_button_press = HIGH;
+int skeet_state = 0; /* 0 = left; 1 = right */
+const int MAX_SKEET_STATE = 1;
+
+/* sound detection */
+const int N_READINGS_PER = 100;
+const int DELAY_PER = 2;
+const int N_READINGS_NOISE = 30;
+const int N_SD_NOISE = 10;
+float noise_ave, noise_sd, noise_threshold;
+float mic_reading;
+
+double servopos[2];
+
 
 void setup()
 {
-  myservo.attach(servopin);
-  myservo.write(90);
-  delay(delay_between);
-  
-  myservo.write(calc_angle(servopos));
-  delay(delay_between);
+  for(int i=0; i<2; i++) {
+    leftservos[i].attach(SERVO_PINS[i]);
+    leftservos[i].write(CENTER[i]);
+  }
+
+  for(int i=0; i<2; i++) {
+    pinMode(LED_PINS[i], OUTPUT);
+    digitalWrite(LED_PINS[i], LOW);
+  }
+
+  pinMode(BUTTON_PIN, INPUT);
+  digitalWrite(BUTTON_PIN, HIGH); /* set pull-up resistor */
+
+  pinMode(LASER_PIN, OUTPUT);
+  digitalWrite(LASER_PIN, LOW);
+
+  pinMode(MIC_PIN, INPUT);
+
+  Serial.begin(9600);
+  getMicBackground(MIC_PIN, &noise_ave, &noise_sd, N_READINGS_NOISE, N_READINGS_PER, DELAY_PER);
+  noise_threshold = noise_sd*N_SD_NOISE;
+  Serial.print("noise ave = ");
+  Serial.println(noise_ave);
+  Serial.print("noise sd = ");
+  Serial.println(noise_sd);
+  Serial.print("noise threshold = ");
+  Serial.println(noise_threshold);
 }
 
 void loop()
 {
-  /* sweep to right */
-  move_servo(myservo, -maxpos, maxpos, n_steps, sec_per_sweep);
-  delay(delay_between);
+  /* read button */
+  button_press = digitalRead(BUTTON_PIN);
 
-  /* sweep to left */
-  move_servo(myservo, maxpos, -maxpos, n_steps, sec_per_sweep);
-  delay(delay_between);
+  if(prev_button_press == HIGH && button_press == LOW) {
+    skeet_state += 1;
+    if(skeet_state > MAX_SKEET_STATE) { skeet_state = 0; }
+    delay(30);
+  }
+  prev_button_press = button_press;
+
+  /* LEDs */
+  if(skeet_state == 0) {
+    digitalWrite(LED_PINS[0], HIGH);
+    digitalWrite(LED_PINS[1], LOW);
+  }
+  else if(skeet_state == 1) {
+    digitalWrite(LED_PINS[0], LOW);
+    digitalWrite(LED_PINS[1], HIGH);
+  }
+  
+  /* read_mic */
+  mic_reading = getMicReading(MIC_PIN, noise_ave, N_READINGS_PER, DELAY_PER);
+  Serial.print(mic_reading);
+  Serial.print(" ");
+  Serial.println(mic_reading / noise_sd);
+  
+  if(mic_reading > noise_threshold) {
+    if(skeet_state==0) {
+      Serial.println("Run from left");
+      runFromLeft();
+    }
+    else if(skeet_state==1) {
+      Serial.println("Run from right");
+      runFromRight();
+    }
+  }
+
 }
+
+void runFromLeft(void) { /* run skeet from left to right */
+  digitalWrite(LASER_PIN, HIGH);
+  delay(1000);
+  digitalWrite(LASER_PIN, LOW);
+  delay(1000);
+
+  /* sweep to right
+  move_servo(myservo, -maxpos, maxpos, n_steps, sec_per_sweep);
+  delay(delay_between); */
+}
+    
+
+void runFromRight(void) { /* run skeet from right to left */
+  digitalWrite(LASER_PIN, HIGH);
+  delay(1000);
+  digitalWrite(LASER_PIN, LOW);
+  delay(1000);
+
+  /* sweep to left
+  move_servo(myservo, maxpos, -maxpos, n_steps, sec_per_sweep);
+  delay(delay_between); */
+}
+    
 
 void move_servo(Servo theservo, double start_position, double end_position, int n_steps, double time_sec)
 {
@@ -62,4 +164,36 @@ void move_servo(Servo theservo, double start_position, double end_position, int 
 int calc_angle(double position)
 {
   return((int)(atan(position) * 180.0/M_PI + 90.0));
+}
+
+/* get a bunch of readings from mic and return the average */
+float getMicReading(int pin, float middle, int nreadings, int delay_time)
+{
+  float result;
+  result = 0.0;
+  for(int i=0; i<nreadings; i++) {
+    result += (abs((float)analogRead(pin) - middle) / (float)nreadings);
+    delay(delay_time);
+  }
+  return(result);
+}
+
+/* measure background noise */
+void getMicBackground(int pin, float *middle, float *sd, int nreadings, int nreadings_per, int delay_time_per)
+{
+  float value;
+  double sumsq;
+
+  *middle = 0.0;
+  sumsq = 0.0;
+
+  for(int i=0; i<nreadings; i++) {
+    
+    value = getMicReading(pin, 0.0, nreadings_per, delay_time_per);
+    (*middle) += value;
+    sumsq += (double)value*(double)value;
+  }
+
+  (*middle) /= (float)nreadings;
+  *sd = sqrt( (sumsq - (*middle)*(*middle)*(float)nreadings)/((float)(nreadings-1)) );
 }
